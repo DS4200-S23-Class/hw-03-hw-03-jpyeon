5e88ea7279eb94a6df12195967810ffc02ba8685
jpyeon <jpyeon1216@gmail.com>
2023-02-07 17:07:59 -0500

    updated files

--- external.js
+++ external.js
@@ -1,1 +1,15 @@
-// external javascript file
+// JS File for HW-03
+
+
+let clickCount = 0;
+
+function buttonClicked(){
+
+    let buttonDiv = document.getElementById("button-div");
+    let counter = clickCount++;
+
+    buttonDiv.innerHTML = "Thanks for Clicking Me " + counter + " " + "Times!";
+
+}
+
+
